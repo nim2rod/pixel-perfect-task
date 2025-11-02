a2dbd0b36c4d2ac91b5f4024c1fce389d84ac0fc
nimrod aviam <nimrod.a6@gmail.com>
2025-11-03 01:02:19 +0200

    hero

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './cmps/Header';
 import NewsBreak from './cmps/NewsBreak';
+import Hero from './cmps/Hero';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <NewsBreak />
+      <Hero />
     </div>
   )
 }
---
+++ src/cmps/Hero.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../styles/cmps/hero.css';
+import heroImage from '../assets/hero/hero-img.png';
+import details from '../assets/hero/details-btn.svg';
+
+const Hero = () => {
+    return (
+        <section className="hero">
+            <img src={heroImage} alt="Hero banner" className="hero__img" />
+
+            <div className="hero__overlay">
+                <div className="hero__quick">
+                    {/* כאן נכניס מאוחר יותר את הקומפוננטה של QuickServices */}
+                    <div className="hero__quick-placeholder">שירותים מהירים</div>
+                </div>
+
+                <div className="hero__content">
+                    <div style={{ fontSize: '2.2rem', fontWeight: '700' }}>
+                        קנט - קרן לביטוח נזקי טבע בחקלאות
+                    </div>
+                    <div style={{ fontSize: '1.6rem' }}>כי לטבע חוקים משלו</div>
+                    <div style={{ fontSize: '1rem' }}>הגן על העסק שלך עם ביטוח של קנט</div>
+                    <img className='more-details' src={details} alt="for more details"/>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
